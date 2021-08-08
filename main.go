6a4ee4ffd064171ca449d2ad3af220b0fd932bbe
solu <soulu@fordeal.com>
2021-08-08 14:45:19 +0800

    case 2

--- main.go
+++ main.go
@@ -6,11 +6,27 @@ import (
 	"net/http"
 )
 
+type Engine struct{}
+
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+	case "/foo":
+		for k, v := range r.Header {
+			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+		}
+	default:
+		fmt.Fprintf(w, "404 NOT FOUND: %s\n", r.URL)
+	}
+}
+
 func main() {
 
-	http.HandleFunc("/", index)
-	http.HandleFunc("/foo", foo)
-	log.Fatal(http.ListenAndServe(":8090", nil))
+	//http.HandleFunc("/", index)
+	//http.HandleFunc("/foo", foo)
+	engine := new(Engine)
+	log.Fatal(http.ListenAndServe(":8090", engine))
 }
 
 func index(resp http.ResponseWriter, req *http.Request) {
